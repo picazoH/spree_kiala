6898885be7b2f791b4148ee34c291fd62acc5548
picazoH <hector@ahaaa.es>
2013-02-25 22:24:20 +0100

    changing kiala_service_url to https protocol

--- app/models/spree/calculator/kiala.rb
+++ app/models/spree/calculator/kiala.rb
@@ -4,7 +4,7 @@ module Spree
     preference :country,  :String,:default  => "ES"
     preference :language,  :String,:default  => "es"
     preference :preparationdelay,  :int,:default  => 1
-    preference :kiala_service_url, :string, :default => 'http://locateandselect.kiala.com/search'
+    preference :kiala_service_url, :string, :default => 'https://locateandselect.kiala.com/search'
 
     attr_accessible :dspid, :country, :language, :preparationdelay, :kiala_service_url
     def self.description
