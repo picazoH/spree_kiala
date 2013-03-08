3c980318afc9e8f5ad7ad10690ee78c64ba811d0
picazoH <hector@ahaaa.es>
2013-03-08 20:30:49 +0100

    adding url attributes

--- app/models/spree/calculator/kiala.rb
+++ app/models/spree/calculator/kiala.rb
@@ -2,12 +2,15 @@ module Spree
   class Calculator::Kiala < Calculator
     preference :dspid,             :string,  :default => 'demo_dsp'
     preference :kiala_service_url, :string,  :default => 'https://locateandselect.kiala.com/search'
+    preference :private_area_url,  :string
+    preference :tracking_url,      :string
     preference :preparationdelay,  :integer, :default => 1
     preference :lower_boundry,     :decimal, :default => 0.0
     preference :upper_boundry,     :decimal, :default => 50.0
     preference :amount,            :decimal, :default => 5.0
 
-    attr_accessible :preferred_dspid, :preferred_kiala_service_url, :preferred_preparationdelay,
+    attr_accessible attr_accessible :preferred_private_area_url, :preferred_tracking_url,
+                    :preferred_dspid, :preferred_kiala_service_url, :preferred_preparationdelay,
                     :preferred_lower_boundry, :preferred_upper_boundry, :preferred_amount
 
     def self.description
